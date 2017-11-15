7ad623ceacc358a97a55790da471a775521a8312
Akio K <akiokaki@umich.edu>
2017-11-15 13:23:14 -0500

    Complete Code

--- SI507project5_tests.py
+++ SI507project5_tests.py
@@ -1,18 +1,17 @@
 import unittest
 from SI507project5_code import *
 
-
-
-
-
-
-
-
-
-
-
-
-
+class Proj5_test_suite(unittest.TestCase):
+    def setUp(self):
+        pass
+    def test_type_is_string(self):
+        pass
+    def test_blog_name_is_string(self):    
+        pass
+    def test_tags_is_list(self):
+        pass
+    def tearDown(self):
+        pass
 
 
 if __name__ == "__main__":
